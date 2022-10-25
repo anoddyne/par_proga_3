c1d55b666b0eaf3dda15422787da8d9b99d73e43
anoddyne <anoddyne@outlook.com>
2022-10-25 17:53:22 +0300

    final version

--- task_besshaposhnikov.c
+++ task_besshaposhnikov.c
@@ -3,43 +3,69 @@
 #include <math.h>
 
 int main() {
-	double x, func, sum, r, fact = 1, first = 1;
-	int n,m;
+	double x, function, sum, lastTerm, temp, factorial = 1, last3, changingSign, error;
+	int i = 1, j, termCount = 0;
+
 	printf("Enter the argument value: ");
 	scanf("%lf", &x);
-	printf("Set the number of summands: ");
-	scanf("%d", &n);
-	func = sin(x);
-	printf("\nThe exact value of the function is equal: %lf", func);
 
+	//Calculating the sum of the Taylor series for the function sin(x)
+	function = sin(x);
+	printf("\nThe exact value of the function is equal: %E\n", function);
+
+	//Calculating the sum of the Taylor series for the function sin(x)
+	lastTerm = x;
 	sum = x;
-	r = x;
-	for (int i = 1; i < n; i++) {
-		r *= -(x * x) / (2 * i * (2 * i + 1));
-		sum += r;
-	}
+	do
+	{
+		lastTerm *= -(x * x) / (2 * i * (2 * i + 1));
+		sum += lastTerm;
+		termCount++;
+		i++;
+	} while (fabs(lastTerm) >= 1e-10);
 
-	printf("\nThe partial sum of the series is equal: %lf\n", sum);
+	//The difference between the answer from math.h and the resulting sum of the Taylor series
+	error = abs(function - sum);
+
+	printf("The resulting sum of the series: %E\n", sum);
+	printf("%d terms of the Taylor series summed\n", termCount);
+	printf("\nThe difference between the answer from math.h and the resulting sum of the Taylor series: %E\n", error);
 
 	printf("\nOutput of the first three terms of the Taylor series: \n");
-
-	r = x;
-	for (int i = 1; i <= 3; i++) {
-		printf("%E\n", r);
-		r *= -(x * x) / (2 * i * (2 * i + 1));
+	temp = x;
+	for (i = 1; i <= 3; i++) {
+		printf("%E\n", temp);
+		temp *= -(x * x) / (2 * i * (2 * i + 1));
 	}
 	printf("\n");
 
-	r = x;
-	m = 2 * n - 6;
+	temp = x;
+
+	/*Let's take the number of terms and subtract from them the last 3 terms that we need to display. 
+	But since even powers and factorials of even numbers are skipped in the series, you will have 
+	to multiply the number obtained earlier by 2.*/
+	last3 = 2 * (termCount - 3);
+
 	printf("Output of the last three terms of the Taylor series: \n");
-	for (int j = 1; j < 2 * n; j++) {
-		fact *= j;
-		if (m < j && j % 2 != 0) {
-			r = first * (pow(x, j)) / (fact);
-			printf("%E\n", r);
-			first *= -1;
-			m++;
+
+	/*If the number of terms is even, then the antepenultimateand last terms will be positive,
+	and the penultimate term will be negative.
+	If the number of terms is odd, then the antepenultimate and last terms will be negative, 
+	and the penultimate term will be positive.*/
+	changingSign = termCount % 2 != 0 ? 1 : -1;
+
+	//Output the last three terms of the Taylor series
+	for (j = 1; j < 2 * termCount; j++) {
+		factorial *= j;
+
+		/*In this condition, we cut off all members of the series, except for the last three, 
+		and even numbers, since in our expansion of the Taylor series there are no even numbers 
+		in powers and factorials of numbers.*/
+		if (last3 < j && j % 2 != 0) {
+			temp = changingSign * (pow(x, j)) / (factorial);
+			printf("%E\n", temp);
+			changingSign *= -1;
+			last3++;
 		}
-		}
+	}
 }
